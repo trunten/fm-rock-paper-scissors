b3d46c28f774fc186dc3722f7b3092ef7cb713f1
Jonathan Davis <trunten@btinternet.com>
2022-12-29 20:28:45 +0000

    Feature: Two Player

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,8 @@
 let score = 0;
+let mode = "";
+let turn = 1;
+
+console.log(document.querySelectorAll("#selected .heading"));
 
 let btns = document.getElementById("selection").getElementsByTagName("span");
 for (let btn of btns) {
@@ -12,62 +16,130 @@ for (let btn of btns) {
         if (window.getComputedStyle(document.getElementById("rules")).display != 'none') {
             document.getElementById("closeBtn").click();    
         }
-        document.getElementById("selection").classList.add("hide");
-        document.getElementById("selected").classList.remove("hide");
         let player = document.getElementById("player")
         let house = document.getElementById("house")
-        let btns = document.getElementById("selection").getElementsByTagName("span");
-        let rndInt = Math.floor(Math.random() * 3)
-        let houseChoice = btns.item(rndInt)
-        player.classList = classes;
-        player.classList.remove("icon");
-        player.classList.add("border");
-        player.childNodes[1].src=img;
-        house.classList = houseChoice.classList
-        house.childNodes[1].src = btns.item(rndInt).childNodes[0].src
+        if (mode == 1) {
+            let btns = document.getElementById("selection").getElementsByTagName("span");
+            let rndInt = Math.floor(Math.random() * 3)
+            let houseChoice = btns.item(rndInt)
+            house.classList = houseChoice.classList
+            house.childNodes[1].src = btns.item(rndInt).childNodes[0].src
+            player.classList = classes;
+            player.classList.remove("icon");
+            player.classList.add("border");
+            player.childNodes[1].src=img;
+        } else {
+            if (turn === 1) {
+                player.classList = classes;
+                player.classList.remove("icon");
+                player.classList.add("border");
+                player.childNodes[1].src=img;
+                turn = 2;
+                document.getElementById("heading").innerText = "Player 2: Pick Your Weapon";
+                alert("Pass to player 2");
+                return;
+            } else {
+                house.classList = classes;
+                house.classList.remove("icon");
+                house.classList.add("border");
+                house.childNodes[1].src=img;
+                turn = 1;
+            }
+        }
+
+        //Hide selection
+        document.getElementById("heading").classList.add("invisible");
+        document.getElementById("selection").classList.add("hide");
+        document.getElementById("selected").classList.remove("hide");
+
         // Win lose draw
         player = player.classList.toString().replace(" ","").replace("border","")
         house = house.classList.toString().replace(" ","").replace("border","")
+        let outcome = ""
         if (player == house) {
-            document.getElementById("draw").classList.remove("hide");
+            outcome = "draw";
         } else if (player == "rock") {
             if (house == "paper") {
                 //lose
-                document.getElementById("lose").classList.remove("hide");
+                outcome = mode == 2 ? "p2" : "lose";
+                score--;
             } else {
                 //win
-                document.getElementById("win").classList.remove("hide");
+                outcome = mode == 2 ? "p1" : "win";
                 score++;
             }
         } else if (player == "paper") {
             if (house == "scissors") {
                 //lose
-                document.getElementById("lose").classList.remove("hide");
+                outcome = mode == 2 ? "p2" : "lose";
+                score--;
             } else {
                 //win
-                document.getElementById("win").classList.remove("hide");
+                outcome = mode == 2 ? "p1" : "win";
                 score++;
             }
         } else if (player == "scissors") {
             if (house == "rock") {
                 //lose
-                document.getElementById("lose").classList.remove("hide");
+                outcome = mode == 2 ? "p2" : "lose";
+                score--;
             } else {
                 //win
-                document.getElementById("win").classList.remove("hide");
+                outcome = mode == 2 ? "p1" : "win";
                 score++;
             }
         }
-        document.getElementById("playerScore").innerText = score;
+        document.getElementById(outcome).classList.remove("hide");
+        if (mode == 1) { document.getElementById("playerScore").innerText = score; }
     });
 }
 
-// Reset game
-document.getElementById("againBtn").addEventListener("click", (e) => {
+//Pick mode
+for (let btn of document.getElementById("mode").children) {
+    btn.addEventListener("click", setGameMode);
+}
+
+function setGameMode(e) {
     e.preventDefault();
+    mode = e.target.getAttribute("mode");
+    let head = document.getElementById("heading");
+    let players = document.querySelectorAll("#selected .heading");
+    if (mode == 1) {
+        head.innerText = "Pick Your Weapon";
+        players[0].innerText = "You Picked"
+        players[1].innerText = "House Picked"
+    } else {
+        head.innerText = "Player 1: Pick Your Weapon";
+        players[0].innerText = "Player 1 Picked"
+        players[1].innerText = "Player 2 Picked"
+        // document.getElementById("score").children[0].innerText = "Two Player"
+        document.getElementById("playerScore").innerText = "#"
+    }
+    reset();
+    e.target.parentNode.classList.add("hide");
+}
+
+// Reset game
+document.getElementById("againBtn").addEventListener("click", reset);
+document.getElementById("fullReset").addEventListener("click", fullReset);
+
+function reset(e) {
+    if (e) { e.preventDefault(); }
+    document.getElementById("p1").classList.add("hide");
+    document.getElementById("p2").classList.add("hide");
     document.getElementById("win").classList.add("hide");
     document.getElementById("lose").classList.add("hide");
     document.getElementById("draw").classList.add("hide");
     document.getElementById("selected").classList.add("hide");
     document.getElementById("selection").classList.remove("hide");
-})
+    document.getElementById("heading").classList.remove("invisible");
+    turn = 1;
+}
+
+function fullReset(e) {
+    reset(e);
+    document.getElementById("selection").classList.add("hide");
+    document.getElementById("heading").classList.add("invisible");
+    document.getElementById("mode").classList.remove("hide");
+    mode = "";
+}
